7dc4a3d6dde4be321b1386efaa7bb373f79e0bf5
RaviSir26 <ravikumarsondhiya260498@gmail.com>
2026-03-10 11:20:53 +0530

    first commit

--- middleware/userJwtAuth.js
+++ middleware/userJwtAuth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken';
 
-const userAuth = async(req, res, next)=>{
+const userAuth = async (req, res, next) => {
 
     try {
-        let bearerHeader = req.headers['Authorization'];
-        
-        if(typeof bearerHeader != 'undefined'){
-            let token = bearerHeader.split(' ')[1];
+        const bearerHeader = req.headers.authorization;
+
+        if (bearerHeader) {
+            const token = bearerHeader.split(" ")[1];
             let user = jwt.verify(token, process.env.JWT_SECRET);
             // console.log(user);
-            res.token = user;
+            req.token = user;
             next();
         }
-        else{
-            res.status(401).json({message: 'Token Not Set'});
+        else {
+            res.status(401).json({ message: 'Token Not Set' });
         }
     } catch (error) {
-        res.status(403).json({message: 'Invalid or Expired Token'});
+        res.status(403).json({ message: 'Invalid or Expired Token' });
     }
 }
 
